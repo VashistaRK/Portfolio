16bafffcdad5eccd6c52a650237b08d43542aa34
VASHISTA RAMAN <218r1a6752@gmail.com>
2025-01-18 19:36:48 +0530

    H

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Add this line to specify the base path
-  build: {
-    rollupOptions: {
-      input: 'index.html',
-    },
-  }
-
+  base: '/Portfolio/', // Ensure this matches your GitHub Pages base path
 });
